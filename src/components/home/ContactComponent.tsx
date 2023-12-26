d0c0e783a5a4ff619823ff1787767b1d3deef9e4
David Caro <david.caro.dec@hotmail.com>
2023-12-26 20:30:37 -0300

    changes required

--- src/components/home/ContactComponent.tsx
+++ src/components/home/ContactComponent.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import * as Icon from "react-feather";
 
 export const ContactComponent = () => {
+
   return (
     <section className="overflow-hidden">
       <div className="container-fluid relative">
@@ -11,7 +12,7 @@ export const ContactComponent = () => {
           <div className="border-0 w-full">
             <iframe
               title="google"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39206.002432144705!2d-95.4973981212445!3d29.709510002925988!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640c16de81f3ca5%3A0xf43e0b60ae539ac9!2sGerald+D.+Hines+Waterwall+Park!5e0!3m2!1sen!2sin!4v1566305861440!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13622.329221377997!2d-58.02625848261718!3d-31.398078899999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95ade8040a01d287%3A0x4705fab9fcbdf5a2!2sDsn%20Empresas!5e0!3m2!1ses!2sar!4v1703631288199!5m2!1ses!2sar"
               style={{ border: "0" }}
               className="h-screen w-full"
               allowFullScreen
@@ -30,7 +31,7 @@ export const ContactComponent = () => {
                 />
               </Link>
               <h3 className="my-6 text-2xl leading-normal font-medium text-center">
-                Get in touch !
+                Mantengamos contacto
               </h3>
 
               <form>
@@ -38,7 +39,7 @@ export const ContactComponent = () => {
                   <div className="lg:col-span-6 mb-5">
                     <div className="text-start">
                       <label htmlFor="name" className="font-semibold">
-                        Your Name:
+                        Su nombre:
                       </label>
                       <div className="form-icon relative mt-2">
                         <Icon.User className="w-4 h-4 absolute top-3 start-4"></Icon.User>
@@ -47,7 +48,7 @@ export const ContactComponent = () => {
                           id="name"
                           type="text"
                           className="form-input ps-11 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0"
-                          placeholder="Name :"
+                          placeholder="Nombre:"
                         />
                       </div>
                     </div>
@@ -56,7 +57,7 @@ export const ContactComponent = () => {
                   <div className="lg:col-span-6 mb-5">
                     <div className="text-start">
                       <label htmlFor="email" className="font-semibold">
-                        Your Email:
+                        Su Email:
                       </label>
                       <div className="form-icon relative mt-2">
                         <Icon.Mail className="w-4 h-4 absolute top-3 start-4"></Icon.Mail>
@@ -65,7 +66,7 @@ export const ContactComponent = () => {
                           id="email"
                           type="email"
                           className="form-input ps-11 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0"
-                          placeholder="Email :"
+                          placeholder="Email:"
                         />
                       </div>
                     </div>
@@ -76,7 +77,7 @@ export const ContactComponent = () => {
                   <div className="mb-5">
                     <div className="text-start">
                       <label htmlFor="subject" className="font-semibold">
-                        Your Question:
+                        Asunto:
                       </label>
                       <div className="form-icon relative mt-2">
                         <Icon.Book className="w-4 h-4 absolute top-3 start-4"></Icon.Book>
@@ -84,7 +85,7 @@ export const ContactComponent = () => {
                           name="subject"
                           id="subject"
                           className="form-input ps-11 w-full py-2 px-3 h-10 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0"
-                          placeholder="Subject :"
+                          placeholder="Asunto: "
                         />
                       </div>
                     </div>
@@ -93,7 +94,7 @@ export const ContactComponent = () => {
                   <div className="mb-5">
                     <div className="text-start">
                       <label htmlFor="comments" className="font-semibold">
-                        Your Comment:
+                        Mensaje:
                       </label>
                       <div className="form-icon relative mt-2">
                         <Icon.MessageCircle className="w-4 h-4 absolute top-3 start-4"></Icon.MessageCircle>
@@ -101,7 +102,7 @@ export const ContactComponent = () => {
                           name="comments"
                           id="comments"
                           className="form-input ps-11 w-full py-2 px-3 h-28 bg-transparent dark:bg-slate-900 dark:text-slate-200 rounded outline-none border border-gray-200 focus:border-indigo-600 dark:border-gray-800 dark:focus:border-indigo-600 focus:ring-0"
-                          placeholder="Message :"
+                          placeholder="Mensaje:"
                         ></textarea>
                       </div>
                     </div>
@@ -113,7 +114,7 @@ export const ContactComponent = () => {
                   name="send"
                   className="py-2 px-5 font-semibold tracking-wide border align-middle duration-500 text-base text-center bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white rounded-md w-full justify-center flex items-center"
                 >
-                  Send Message
+                  Mandar mensaje
                 </button>
               </form>
             </div>
